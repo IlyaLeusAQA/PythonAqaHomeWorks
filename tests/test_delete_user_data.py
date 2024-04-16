191f6d98e407e6645f2c3720e2a9f075bf53d8ab
Ilya Leus <ilyaleus@gmail.com>
2024-04-16 22:20:07 +0700

    add test_delete_user

--- tests/test_delete_user_data.py
+++ tests/test_delete_user_data.py
@@ -8,7 +8,7 @@ BASE_URL = "https://reqres.in/api/users/2"
 @allure.title('Удаление тестового юзера по id')
 def test_delete_user_data():
     with allure.step(f'Отправляем запрос по адресу: {BASE_URL}'):
-        response = httpx.patch(BASE_URL, json=body)
+        response = httpx.delete(BASE_URL)
 
     with allure.step(f'Проверяем код ответа'):
-        assert response.status_code == 200
+        assert response.status_code == 204
