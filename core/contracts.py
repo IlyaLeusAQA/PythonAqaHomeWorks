b5cd9d09c7555d784209dc871162c787bc1262cb
Ilya Leus <ilyaleus@gmail.com>
2024-04-16 20:30:13 +0700

    add create_user_scheme

--- core/contracts.py
+++ core/contracts.py
@@ -21,3 +21,15 @@ RESOURCE_DATA_SCHEMA = {
     },
     "required": ["id", "name", "year", "color", "pantone_value"]
 }
+
+CREATED_USER_SCHEME = {
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "job": {"type": "string"},
+        "id": {"type": "string"},
+        "createdAt": {"type": "string"}
+
+    },
+    "required": ["id", "createdAt"]
+}
